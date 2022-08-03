6a884f248c69d9db874b4c25849962c5d8054106
Matt Ballard <mattjballard81@gmail.com>
2022-08-04 07:17:23 +0900

    add search header component with routing to search results page

---
+++ components/SearchHeader.js
@@ -0,0 +1,50 @@
+import { useRef } from "react";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { MicrophoneIcon, SearchIcon, XIcon } from "@heroicons/react/solid";
+import User from "./User";
+
+export default function SearchHeader() {
+  const router = useRouter();
+  const searchRef = useRef(null);
+
+  const search = e => {
+    e.preventDefault();
+    const term = searchRef.current.value.trim();
+    if (!term) return;
+    router.push(`/search?term=${term}`);
+  };
+
+  return (
+    <header className='sticky top-0 bg-white'>
+      <div className='flex w-full p-6 items-center'>
+        <Image
+          onClick={() => router.push("/")}
+          src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2560px-Google_2015_logo.svg.png'
+          objectFit='contain'
+          width='120'
+          height='40'
+          alt='Google Logo'
+          className='cursor-pointer'
+        />
+        <form className='flex border border-gray-200 rounded-full shadow-lg px-6 py-3 ml-10 mr-5 flex-grow max-w-3xl items-center'>
+          <input
+            type='text'
+            defaultValue={router.query.term}
+            ref={searchRef}
+            className='w-full focus:outline-none'
+          />
+          <XIcon
+            className='h-7 text-gray-500 cursor-pointer sm:mr-3'
+            onClick={() => (searchRef.current.value = "")}
+          />
+          <MicrophoneIcon className='h-6 hidden sm:inline-flex text-blue-500 pl-4 border-l-2 border-gray-300 mr-3' />
+          <SearchIcon className='h-6 hidden sm:inline-flex text-blue-500' />
+          <button type='submit' onClick={search} hidden></button>
+        </form>
+
+        <User className='ml-auto whitespace-nowrap' />
+      </div>
+    </header>
+  );
+}
--- components/User.js
+++ components/User.js
@@ -1,7 +1,7 @@
 // import Image from "next/image";
 import { useSession, signIn, signOut } from "next-auth/react";
 
-export default function User() {
+export default function User({ className }) {
   const { data: session } = useSession();
 
   if (session) {
@@ -11,7 +11,7 @@ export default function User() {
           onClick={signOut}
           src={session.user.image}
           alt=''
-          className='h-10 w-10 rounded-full hover:bg-gray-200 cursor-pointer p-1'
+          className={`h-10 w-10 rounded-full hover:bg-gray-200 cursor-pointer p-1 ${className}`}
         />
       </>
     );
@@ -19,7 +19,7 @@ export default function User() {
   return (
     <>
       <button
-        className='bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:shadow-md hover:brightness-105'
+        className={`bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:shadow-md hover:brightness-105 ${className}`}
         onClick={signIn}>
         Sign In
       </button>
